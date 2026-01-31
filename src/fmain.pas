851b3700e34f0fbc12bed9fd19abd169c828ab64
michaeltanghus <michael@marmic.dk>
2026-01-31 18:46:32 +0100

    With errors - Main window UI with menu, results/log tabs, and logging

--- src/fmain.pas
+++ src/fmain.pas
@@ -1,39 +1,74 @@
-unit fmain;
+unit fMain;
 
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls, StdCtrls;
 
 type
-
-  { TForm1 }
-
-  TForm1 = class(TForm)
+  { TMainForm }
+  TMainForm = class(TForm)
     MainMenu1: TMainMenu;
-    MenuItem1: TMenuItem;
-    MenuItem2: TMenuItem;
-    MenuItem3: TMenuItem;
-    MenuItem4: TMenuItem;
-    MenuItem5: TMenuItem;
-    MenuItem6: TMenuItem;
-    MenuItem7: TMenuItem;
-    MenuItem8: TMenuItem;
-    MenuItem9: TMenuItem;
+    MemoLog: TMemo;
+    PageControl1: TPageControl;
+    TabLog: TTabSheet;
+    TabResults: TTabSheet;
+
+    // Menu items (names must match what you created in the designer)
+    mnuFile: TMenuItem;
+    mnuFileNewSearch: TMenuItem;
+    mnuFileOpenSearch: TMenuItem;
+    mnuFileSaveSearch: TMenuItem;
+    mnuFileSaveSearchAs: TMenuItem;
+    mnuFileExport: TMenuItem;
+    mnuFileExit: TMenuItem;
+
+    mnuSettings: TMenuItem;
+    mnuSettingsPreferences: TMenuItem;
+
+    procedure FormCreate(Sender: TObject);
+    procedure mnuFileExitClick(Sender: TObject);
+    procedure mnuFileNewSearchClick(Sender: TObject);
   private
-
+    procedure Log(const AMessage: string);
   public
-
   end;
 
 var
-  Form1: TForm1;
+  TMainForm: TMainForm;
 
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  PageControl1.ActivePage := TabResults;
+  Log('Application started.');
+end;
+
+procedure TMainForm.mnuFileExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TMainForm.mnuFileNewSearchClick(Sender: TObject);
+begin
+  Log('New Search clicked (dialog not implemented yet).');
+  // Next commit: show Search dialog and run scan
+end;
+
+procedure TMainForm.Log(const AMessage: string);
+var
+  TimeStamp: string;
+begin
+  TimeStamp := FormatDateTime('yyyy-mm-dd hh:nn:ss', Now);
+  MemoLog.Lines.Add(TimeStamp + '  ' + AMessage);
+end;
 
 end.
 
