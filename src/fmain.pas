21cc5164dc1debcd8d8df7d6fd6645277aebd8ec
michaeltanghus <michael@marmic.dk>
2026-01-31 17:31:28 +0100

    Add main window UI skeleton (menu, results/log tabs)

--- src/fmain.pas
+++ src/fmain.pas
@@ -5,10 +5,23 @@ unit fmain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
   private
 
   public
